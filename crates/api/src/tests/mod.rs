98b94b0bed5287ecd3260420a12106dbaeb572e0
bonedaddy <alex@bonedaddy.io>
2025-03-11 02:57:48 -0700

    add test for the initialize api

--- crates/api/src/tests/mod.rs
+++ crates/api/src/tests/mod.rs
@@ -1,1 +1,58 @@
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{
+    signature::Keypair, signer::Signer, system_instruction, transaction::Transaction,
+};
+use spl_token_2022::{extension::ExtensionType, state::Mint};
+use spl_token_client::token::ExtensionInitializationParams;
+
 pub mod test_initialize;
+
+/// Creates a token mint with the ConfidentialTransferMint extension
+pub async fn create_confidential_mint(rpc: &RpcClient, mint: &Keypair, authority: &Keypair) {
+    let space = ExtensionType::try_calculate_account_len::<Mint>(&[
+        ExtensionType::ConfidentialTransferMint,
+    ])
+    .unwrap();
+    let rent = rpc
+        .get_minimum_balance_for_rent_exemption(space)
+        .await
+        .unwrap();
+
+    let create_account_ix = system_instruction::create_account(
+        &authority.pubkey(),
+        &mint.pubkey(),
+        rent,
+        space as u64,
+        &spl_token_2022::id(),
+    );
+
+    let extension_init_ix = ExtensionInitializationParams::ConfidentialTransferMint {
+        authority: Some(authority.pubkey()),
+        auto_approve_new_accounts: true,
+        auditor_elgamal_pubkey: None,
+    }
+    .instruction(&spl_token_2022::id(), &mint.pubkey())
+    .unwrap();
+
+    let mut tx = Transaction::new_with_payer(
+        &[
+            create_account_ix,
+            extension_init_ix,
+            spl_token_2022::instruction::initialize_mint(
+                &spl_token_2022::id(),
+                &mint.pubkey(),
+                &authority.pubkey(),
+                None,
+                9,
+            )
+            .unwrap(),
+        ],
+        Some(&authority.pubkey()),
+    );
+    tx.sign(
+        &vec![authority, mint],
+        rpc.get_latest_blockhash().await.unwrap(),
+    );
+
+    rpc.send_and_confirm_transaction(&tx).await.unwrap();
+}
--- crates/api/src/tests/test_initialize.rs
+++ crates/api/src/tests/test_initialize.rs
@@ -1,24 +1,72 @@
 use std::sync::Arc;
 
-use common::test_helpers::test_key;
+use crate::{router, tests::create_confidential_mint, types::Initialize};
+use axum::body::Body;
+use base64::{prelude::BASE64_STANDARD, Engine};
+use common::{
+    key_generator::{derive_ae_key, derive_elgamal_key, KeypairType},
+    test_helpers::test_key,
+};
 use http::Request;
+use http_body_util::BodyExt;
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::{
+    commitment_config::CommitmentConfig, signature::Keypair, signer::Signer,
+    transaction::Transaction,
+};
 use tower::ServiceExt;
-
-use crate::router;
 
 #[tokio::test]
 async fn test_initialize() {
     let key = test_key();
+    let mint = Keypair::new();
+    let user_ata = spl_associated_token_account::get_associated_token_address_with_program_id(
+        &key.pubkey(),
+        &mint.pubkey(),
+        &spl_token_2022::id(),
+    );
+    let rpc = Arc::new(RpcClient::new_with_commitment(
+        "http://localhost:8899".to_string(),
+        CommitmentConfig::confirmed(),
+    ));
 
-    let router = router::new(Arc::new(RpcClient::new(
-        "https://api.devnet.solana.com".to_string(),
-    )));
+    rpc.request_airdrop(&key.pubkey(), 100_000_000_000)
+        .await
+        .unwrap();
 
-    /*router.oneshot(
-        Request::builder()
-        .method("POST")
-        .uri("/confidential-balances/initialize")
-        .body
-    );*/
+    create_confidential_mint(&rpc, &mint, &key).await;
+
+    let elgamal_sig = key.sign_message(&KeypairType::ElGamal.message_to_sign(user_ata));
+    let ae_sig = key.sign_message(&KeypairType::Ae.message_to_sign(user_ata));
+    let elgamal_key = derive_elgamal_key(elgamal_sig).unwrap();
+    let ae_key = derive_ae_key(ae_sig).unwrap();
+
+    let init = Initialize {
+        authority: key.pubkey(),
+        token_mint: mint.pubkey(),
+        elgamal_signature: elgamal_sig,
+        ae_signature: ae_sig,
+    };
+
+    let router = router::new(rpc.clone());
+
+    let res = router
+        .oneshot(
+            Request::builder()
+                .method("POST")
+                .uri("/confidential-balances/initialize")
+                .header("Content-Type", "application/json")
+                .body(Body::from(serde_json::to_string(&init).unwrap()))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    let body = res.into_body().collect().await.unwrap().to_bytes();
+    let body_str: String = serde_json::from_slice(&body[..]).unwrap();
+    let mut tx: Transaction =
+        bincode::deserialize(&BASE64_STANDARD.decode(body_str).unwrap()).unwrap();
+
+    tx.sign(&vec![&key], rpc.get_latest_blockhash().await.unwrap());
+
+    rpc.send_and_confirm_transaction(&tx).await.unwrap();
 }
