c7d0b21257f571618efdc64b971285649227c2f2
bonedaddy <alex@bonedaddy.io>
2025-04-07 22:04:05 -0700

    create unwrapped token account if it does not exist during unwrap

--- crates/api/src/handlers/unwrap_tokens.rs
+++ crates/api/src/handlers/unwrap_tokens.rs
@@ -28,27 +28,36 @@ pub async fn unwrap_tokens(
             &spl_token_2022::id(),
         );
     let wrapped_mint_authority = get_wrapped_mint_authority(&payload.wrapped_token_mint);
-    let ix = spl_token_wrap::instruction::unwrap(
-        &spl_token_wrap::id(),
-        &spl_associated_token_account::get_associated_token_address_with_program_id(
+
+    let mut ixs = vec![
+        spl_associated_token_account::instruction::create_associated_token_account_idempotent(
+            &payload.authority,
+            &payload.authority,
+            &payload.unwrapped_token_mint,
+            &payload.unwrapped_token_program
+        ),
+        spl_token_wrap::instruction::unwrap(
+            &spl_token_wrap::id(),
+            &spl_associated_token_account::get_associated_token_address_with_program_id(
+                &wrapped_mint_authority,
+                &payload.unwrapped_token_mint,
+                &payload.unwrapped_token_program,
+            ),
+            &unwrapped_user_ata,
             &wrapped_mint_authority,
             &payload.unwrapped_token_mint,
+            &spl_token_2022::id(),
             &payload.unwrapped_token_program,
-        ),
-        &unwrapped_user_ata,
-        &wrapped_mint_authority,
-        &payload.unwrapped_token_mint,
-        &spl_token_2022::id(),
-        &payload.unwrapped_token_program,
-        &wrapped_user_ata,
-        &payload.wrapped_token_mint,
-        &payload.authority,
-        &[],
-        payload.amount,
-    );
+            &wrapped_user_ata,
+            &payload.wrapped_token_mint,
+            &payload.authority,
+            &[],
+            payload.amount,
+        )
+    ];
 
     let tx = match bincode::serialize(&Transaction::new_with_payer(
-        &[ix],
+        &ixs,
         Some(&payload.authority),
     )) {
         Ok(tx) => tx,
